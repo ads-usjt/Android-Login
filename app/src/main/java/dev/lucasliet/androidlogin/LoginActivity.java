fa55ddba684cdef2fb80ec350fa8165a7814fb84
lucasliet <lucasouliveira@gmail.com>
2020-09-25 21:49:52 -0300

    Disable Login button when not signed

--- app/src/main/java/dev/lucasliet/androidlogin/LoginActivity.java
+++ app/src/main/java/dev/lucasliet/androidlogin/LoginActivity.java
@@ -5,13 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.orhanobut.hawk.Hawk;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView textViewSignUp;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Hawk.init(this).build();
+
+        textViewSignUp = findViewById(R.id.textViewSignUp);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        if (Hawk.contains("tem_cadastro")){
+            if (Hawk.get("tem_cadastro")){
+                enableLogin();
+            }
+        } else {
+            disableLogin();
+        }
+    }
+
+    private void enableLogin() {
+        textViewSignUp.setVisibility(View.GONE);
+        buttonLogin.setEnabled(true);
+        buttonLogin.setBackgroundColor(getColor(R.color.colorPrimary));
+    }
+
+    private void disableLogin() {
+        textViewSignUp.setVisibility(View.VISIBLE);
+        buttonLogin.setEnabled(false);
+        buttonLogin.setBackgroundColor(getColor(R.color.disabled));
     }
 
     public void SignUp(View view) {
