7f57e1fde8e86f9dc6cc61fd8135b5630f249aa9
lsoliveira <lsoliveira@magnasistemas.com.br>
2020-10-02 20:18:18 -0300

    User Repository

---
+++ app/src/main/java/dev/lucasliet/androidlogin/model/UserRepository.java
@@ -0,0 +1,21 @@
+package dev.lucasliet.androidlogin.model;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+public class UserRepository {
+    private UserDAO userDAO;
+    private LiveData<User> user;
+    UserRepository(Application application) {
+        AlunoRoomDatabase db = AlunoRoomDatabase.getDatabase(application);
+        userDAO = db.userDAO();
+        user = userDAO.getUser();
+    }
+    LiveData<User> getUsuario() {
+        return user;
+    }
+    void insert(User user) {
+        AlunoRoomDatabase.databaseWriteExecutor.execute(() -> userDAO.insert(user));
+    }
+}
