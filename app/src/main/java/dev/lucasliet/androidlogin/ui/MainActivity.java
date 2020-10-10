43e6ddc9eaa476ca36cbea277884b81d18cfe608
lucasliet <lucasouliveira@gmail.com>
2020-10-09 21:07:20 -0300

    Bottom Nav Bar Start

--- app/src/main/java/dev/lucasliet/androidlogin/ui/MainActivity.java
+++ app/src/main/java/dev/lucasliet/androidlogin/ui/MainActivity.java
@@ -1,11 +1,17 @@
 package dev.lucasliet.androidlogin.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
 
 import dev.lucasliet.androidlogin.R;
 import dev.lucasliet.androidlogin.factory.ActionBarFactory;
@@ -16,8 +22,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActionBarFactory.createActionBar(getSupportActionBar());
+        ActionBarFactory.createActionBar(Objects.requireNonNull(getSupportActionBar()));
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+
+        bottomNav.setOnNavigationItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    Log.d("NAVMENU","openFragment(new HomeFragment())");
+                    return true;
+
+                case R.id.contact:
+                    openFragment(new TrendFragment());
+                    return true;
+
+                case R.id.profile:
+                    openFragment(new AccountFragment());
+                    return true;
+
+                case R.id.setting:
+                    openFragment(new SettingsFragment());
+                    return true;
+
+            }
+
+            return false;
+        });
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
